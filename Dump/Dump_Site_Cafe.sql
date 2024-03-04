85ed9cc3e48bbcaa2fac4c5d563f38b1a9ed4876
quemet <129742514+quemet@users.noreply.github.com>
2024-03-04 14:43:47 +0100

    Update Dump_Site_Cafe.sql

--- Dump/Dump_Site_Cafe.sql
+++ Dump/Dump_Site_Cafe.sql
@@ -8,7 +8,7 @@ CREATE TABLE Utilisateur(
    Nom_Utilisateur VARCHAR(100) NOT NULL,
    Prenom_Utilisateur VARCHAR(100) NOT NULL,
    EstAdmin BOOLEAN NOT NULL,
-   MotDePasse_utilisateur VARCHAR(256) NOT NULL,
+   MotDePasse_Utilisateur VARCHAR(256) NOT NULL,
    PRIMARY KEY(Id_Utilisateur),
    UNIQUE(Pseudo_Utilisateur)
 );
